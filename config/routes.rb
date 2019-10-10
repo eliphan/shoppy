b3a9f46e311e0f16a5bda0d873b5ba8dec9c8c11
eliphan <aliceconnic@gmail.com>
2019-10-09 21:09:26 -0700

    add admin function

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_for :admins
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:index, :show]
   resouces :orders, only: [:index, :show, :create]
   resouces :order_items, only: [:index. :show, :create, :edit, :update, :destroy]
+  end
+
+  namespace :admin do
+    resouces :products, only: [:index, :new, :create, :edit, :update, :destroy]
+    resouces :categories, :only [:index, :new, :create, :edit, :update, :destroy]
+    resouces :orders, only: [:index]
+  end
 
 end
